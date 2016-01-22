f397b9333b351346872b741dd67bc8515bdacfb4
shybyte <Marco.Stahl@gmx.de>
2016-01-22 09:29:48 +0100

    CommandQueue

--- src/server/methods.ts
+++ src/server/methods.ts
@@ -1,13 +1,26 @@
+interface Command {
+  methodName: string;
+  args: any[];
+}
+
+const commandQueue: Command[] = [];
+
+function executeNextCommandInQueue() {
+  const command = commandQueue.shift();
+  if (command) {
+    const method: Function = (serverMethods as any)[command.methodName];
+    // console.log('Executing Command: ', command.methodName, command.args);
+    method(...command.args);
+  }
+  Meteor.setTimeout(executeNextCommandInQueue, 1);
+}
+
+executeNextCommandInQueue();
+
 const serverMethods: ServerMethods = {
-  createEntity(entity: Entity) {
-    EntitiesFacade.insert(entity);
-  },
-  updateEntity(_id: string, entity: Entity) {
-    EntitiesFacade.update(_id, entity);
-  },
-  deleteEntity(_id: string) {
-    EntitiesFacade.deleteEntity(_id);
-  },
+  createEntity: EntitiesFacade.insert,
+  updateEntity: EntitiesFacade.update,
+  deleteEntity: EntitiesFacade.deleteEntity,
   createField(field: DataCategory) {
     DataCategories.insert(field);
   },
@@ -16,4 +29,8 @@ const serverMethods: ServerMethods = {
   },
 };
 
-Meteor.methods(serverMethods);
+const queuedServerMethods = _.mapValues(serverMethods, (method, methodName) => (...args: any[]) => {
+  commandQueue.push({methodName, args});
+});
+
+Meteor.methods(queuedServerMethods);
